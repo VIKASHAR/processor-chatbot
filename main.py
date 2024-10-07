c0afbea96693d2bfefc715266caa9cf19f2a8a4e
Vikash <119405655+VIKASHAR@users.noreply.github.com>
2024-10-07 18:11:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
 import streamlit as st
-from sklearnex import patch_sklearn, config_context 
-patch_sklearn()
-import streamlit as st
 import pandas as pd
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
